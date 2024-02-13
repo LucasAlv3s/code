d571e2de51e0a6576c09b977fb6429f76dc9334c
Lucas Alves <80328704+LucasAlv3s@users.noreply.github.com>
2024-02-13 12:05:12 -0300

    InnerHTML

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 // JavaScript Externo
 // Sintaxe em JavaScript
+/*
 alert("Um alerta rodando de uma página externa");
+*/
+
+document.getElementById("text").innerHTML="Meu primeiro texto <b>JS</b>!";
